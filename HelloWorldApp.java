47d88eb80773817b4d681d47cd905751799d09c0
kha333n <44539272+kha333n@users.noreply.github.com>
2020-10-17 12:43:42 +0500

    Git Setup

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,6 +1,6 @@
 public class HelloWorldApp {
 
-	public static void public static void main(String[] args) {
+	public static void main(String[] args) {
 		
 		System.out.println("Hello World!");
 	}
